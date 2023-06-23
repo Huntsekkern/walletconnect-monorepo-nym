07e10a03fb1819e7b633a2b3a0b3cd2f94a824e5
Huntsekker <r****@nymtech.net>
2023-06-23 15:11:57 +0200

    Change which single-test is used to have one which properly sends subscription across the mixnet, without mocking

--- packages/core/single-test/relayer.spec.ts
+++ packages/core/single-test/relayer.spec.ts
@@ -32,26 +32,30 @@ describe("Relayer", () => {
     relayer = core.relayer;
   });
 
-  afterEach(async () => {
-    await disconnectSocket(core.relayer);
-  });
+  // afterEach(async () => {
+  //   await disconnectSocket(core.relayer);
+  // });
 
   describe("subscribe", () => {
-    // let relayer;
-    // beforeEach(async () => {
-    //   relayer = new Relayer({
-    //     core,
-    //     logger,
-    //     relayUrl: TEST_CORE_OPTIONS.relayUrl,
-    //     projectId: TEST_CORE_OPTIONS.projectId,
-    //   });
-    //   await relayer.init();
-    // });
-    // afterEach(async () => {
-    //   await disconnectSocket(relayer);
-    // });
+    let relayer;
+    beforeEach(async () => {
+      await disconnectSocket(core.relayer);
+      relayer = new Relayer({
+        core,
+        logger,
+        relayUrl: TEST_CORE_OPTIONS.relayUrl,
+        projectId: TEST_CORE_OPTIONS.projectId,
+      });
+      await relayer.init();
+    });
+    afterEach(async () => {
+      await disconnectSocket(relayer);
+    });
 
-    it("returns the id provided by calling `subscriber.subscribe` with the passed topic", async () => {
+    // This test do not check that the SP/Relay correctly receives the message.
+    // Actually worse, the subscribe function is mocked and never executed!
+    // But connection to the relay works properly through the SP!! It's just not sending the subscribe.
+/*    it("returns the id provided by calling `subscriber.subscribe` with the passed topic", async () => {
       const spy = Sinon.spy(() => "mock-id");
       relayer.subscriber.subscribe = spy;
       let id;
@@ -68,6 +72,24 @@ describe("Relayer", () => {
       // @ts-expect-error
       expect(spy.calledOnceWith("abc123")).to.be.true;
       expect(id).to.eq("mock-id");
+    });*/
+
+    // While this test do not properly check what is return as a message, my client logs the received message (currently)
+    // And the relay do properly send responses through the SP, and then through the service provider.
+    it("should be able to resubscribe on topic that already exists", async () => {
+      const topic = generateRandomBytes32();
+      const id = await relayer.subscribe(topic);
+      const expectedId = hashMessage(topic + (await core.crypto.getClientId()));
+      const a = await relayer.subscribe(topic);
+      const b = await relayer.subscribe(topic);
+      const c = await relayer.subscribe(topic);
+      expect(a).to.equal(id);
+      expect(a).to.equal(b);
+      expect(b).to.equal(c);
+      expect(a).to.equal(expectedId);
+      expect(b).to.equal(expectedId);
+      expect(c).to.equal(expectedId);
+      expect(id).to.equal(expectedId);
     });
   });
 });
